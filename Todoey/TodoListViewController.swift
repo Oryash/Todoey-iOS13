a8e6293780b3200e256d15d8e6b429522f52ad7e
Marina Svistkova <m.svistkova@ritm.media>
2022-10-26 13:10:38 +0300

    At the stage of adding and removing checkmark when tapping the row

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["Find Mike", "Buy Eggos", "Kill Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+    }
+
+    //MARK: TableView DataSource Methods
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+
+
+        cell.textLabel?.text = itemArray[indexPath.row]
+
+        return cell
+    }
+
+    //MARK: TableView Delegate Methods
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+//        print(itemArray[indexPath.row])
+
+        //выделяет чекмарком ячейку
+
+        if tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark {
+
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+
+        else {
+            
+        }
+
+        //убирает выделение строки с анимацией затухания.
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
+
+
